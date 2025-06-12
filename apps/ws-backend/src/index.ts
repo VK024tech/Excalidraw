1eabec9838e46aae2cb4744fb609787524ad1194
Vivek Kandari <Kandarivivek260@gmail.com>
2025-06-12 17:56:11 +0530

    Revert "backend add"
    This reverts commit 147440d140dbc0d115b4778dd423206320839c73.

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -2,7 +2,6 @@ import WebSocket, { WebSocketServer } from "ws";
 import Jwt, { JwtPayload } from "jsonwebtoken";
 
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { prismaClient } from "@repo/db/client";
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -15,22 +14,17 @@ interface User {
 const users: User[] = [];
 
 function checkUser(token: string): string | null {
-  try {
-    const decoded = Jwt.verify(token, JWT_SECRET);
+  const decoded = Jwt.verify(token, JWT_SECRET);
 
-    if (typeof decoded == "string") {
-      return null;
-    }
-
-    if (!decoded || !(decoded as JwtPayload).userId) {
-      return null;
-    }
-
-    return decoded.userId;
-  } catch (error) {
+  if (typeof decoded == "string") {
     return null;
   }
-  return null;
+
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    return null;
+  }
+
+  return decoded.userId;
 }
 
 wss.on("connection", function connection(ws, request) {
@@ -53,7 +47,7 @@ wss.on("connection", function connection(ws, request) {
     ws,
   });
 
-  ws.on("message", async function message(data) {
+  ws.on("message", function message(data) {
     const parsedData = JSON.parse(data as unknown as string);
     if (parsedData.type === "join_room") {
       const user = users.find((x) => x.ws === ws);
@@ -64,7 +58,7 @@ wss.on("connection", function connection(ws, request) {
       if (!user) {
         return;
       }
-      user.rooms = user?.rooms.filter((x) => x === parsedData.roomId);
+      user.rooms = user?.rooms.filter((x) => x === parsedData.room);
     }
 
     if (parsedData.type === "chat") {
@@ -80,14 +74,6 @@ wss.on("connection", function connection(ws, request) {
             })
           );
         }
-      });
-
-      await prismaClient.chat.create({
-        data: {
-          roomId,
-          message,
-          userId,
-        },
       });
     }
   });
