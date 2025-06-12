147440d140dbc0d115b4778dd423206320839c73
Vivek Kandari <Kandarivivek260@gmail.com>
2025-06-12 13:31:48 +0530

    backend add

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -2,6 +2,7 @@ import WebSocket, { WebSocketServer } from "ws";
 import Jwt, { JwtPayload } from "jsonwebtoken";
 
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { prismaClient } from "@repo/db/client";
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -14,17 +15,22 @@ interface User {
 const users: User[] = [];
 
 function checkUser(token: string): string | null {
-  const decoded = Jwt.verify(token, JWT_SECRET);
+  try {
+    const decoded = Jwt.verify(token, JWT_SECRET);
 
-  if (typeof decoded == "string") {
+    if (typeof decoded == "string") {
+      return null;
+    }
+
+    if (!decoded || !(decoded as JwtPayload).userId) {
+      return null;
+    }
+
+    return decoded.userId;
+  } catch (error) {
     return null;
   }
-
-  if (!decoded || !(decoded as JwtPayload).userId) {
-    return null;
-  }
-
-  return decoded.userId;
+  return null;
 }
 
 wss.on("connection", function connection(ws, request) {
@@ -47,7 +53,7 @@ wss.on("connection", function connection(ws, request) {
     ws,
   });
 
-  ws.on("message", function message(data) {
+  ws.on("message", async function message(data) {
     const parsedData = JSON.parse(data as unknown as string);
     if (parsedData.type === "join_room") {
       const user = users.find((x) => x.ws === ws);
@@ -58,7 +64,7 @@ wss.on("connection", function connection(ws, request) {
       if (!user) {
         return;
       }
-      user.rooms = user?.rooms.filter((x) => x === parsedData.room);
+      user.rooms = user?.rooms.filter((x) => x === parsedData.roomId);
     }
 
     if (parsedData.type === "chat") {
@@ -74,6 +80,14 @@ wss.on("connection", function connection(ws, request) {
             })
           );
         }
+      });
+
+      await prismaClient.chat.create({
+        data: {
+          roomId,
+          message,
+          userId,
+        },
       });
     }
   });
